bf277c7520dbda8ba4b69bd0bb3e078362509054
Dawa Bokhang <dawabokhang@Dawas-MacBook-Pro.local>
2018-01-06 16:45:24 -0500

    Included Gulp CSS Workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 // always remember to require the packages you need in order to use the code
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss')
+var autoprefixer = require('autoprefixer');
+var cssVars = require('postcss-simple-vars');
+var nestedCss = require('postcss-nested');
 
-// EVERYTHING IN GULP REVOLVES AROUND TASKS
+////////// EVERYTHING IN GULP REVOLVES AROUND TASKS //////////
 
 // the task method takes in 2 parameters
   // 1) what you want to name the Task
@@ -16,14 +20,26 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-  console.log('CSS Task');
+
+  // always remember to include the return keyword for gulp.src
+  // gulp.src points to css file
+  return gulp.src('./app/assets/styles/styles.css')
+
+    // pass in the post css automation tools you want run as an array into the postcss.([]) method as an array
+    // here you passed in the css variable, nested css and auto prefixer packages
+    .pipe(postcss([cssVars, autoprefixer, nestedCss]))
+
+    // gulp.dest points to where we want the new css file to be made
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
+
   // the watch plugin takes 2 parameters
     // 1) the location of the file you want to watch for
     // 2) what you want the watch function to do
   watch('./app/index.html', function(){
+
     // the start method runs the name of the task that is passed in
     gulp.start('html');
   });
